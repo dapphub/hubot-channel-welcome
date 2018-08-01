942b8282f3de97a4d814418b79194606f631c9aa
@ianh <ith.harvey@gmail.com>
2018-08-01 13:27:27 -0500

    fixed routing through index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-
-const path = require('path');
-
-module.exports = function (robot) {
-  var scriptsPath = path.resolve(__dirname, 'scripts');
-  return [
-    robot.loadFile(scriptsPath, 'hubotDmChannelWelcome.js')
-  ];
-};
+module.exports = require('./scripts/hubotDmChannelWelcome.js')
