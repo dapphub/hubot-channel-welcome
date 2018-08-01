ea497536e29e64222f25736625f5ba92647006c9
@ianh <ith.harvey@gmail.com>
2018-08-01 13:45:01 -0500

    changed main function call

--- scripts/hubotDmChannelWelcome.js
+++ scripts/hubotDmChannelWelcome.js
@@ -11,7 +11,7 @@ const {RBU} = require('hubot-doge-utility-functions')
 
 //authToken that we got from the Rocket.Chat API (requires
 //process.env.ROCKETCHAT_PASSWORD, process.env.ROCKETCHAT_USER and process.env.ROCKETCHAT_URL to be set)
-function runWebhookWelcomeDM() {
+function hubotWelcomeDM() {
   RBU.getAuthToken().then(res => {
     console.log('authToken -> ', res)
     const authToken = res.data.authToken
@@ -110,4 +110,4 @@ function runWebhookWelcomeDM() {
     })
   })
 }
-module.exports = {runWebhookWelcomeDM}
+module.exports = {hubotWelcomeDM}
